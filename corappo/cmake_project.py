6bea8c1f883c2fb95fc28630b1dd251cc022ae95
Matthew D. Scholefield <matthew331199@gmail.com>
2019-12-04 21:46:54 -0600

    Support defines and C C support isn't perfect though due to things like CMAKE_C_FLAGS

--- corappo/cmake_project.py
+++ corappo/cmake_project.py
@@ -14,6 +14,7 @@ def parse_compiler_args(args):
     parser.add_argument('-g', dest='debug', action='store_true')
     parser.add_argument('-c', dest='compile_only', action='store_true')
     parser.add_argument('-O', dest='optimize', type=int)
+    parser.add_argument('-D', dest='defines', action='append')
     parser.add_argument('-std', dest='standard')
     parser.add_argument('-W', dest='warn')
     parser.add_argument('-l', dest='libs', action='append')
@@ -23,7 +24,7 @@ def parse_compiler_args(args):
     args.libs = args.libs or []
     args.libs.extend(['${CMAKE_CURRENT_LIST_DIR}/' + i for i in remaining if i.endswith('.a')])
     args.flags = [i for i in remaining if i.startswith('-')]
-    args.inputs = [i for i in remaining if re.match(r'.+\.[^.]+', i) and not i.endswith('.a')]
+    args.inputs = [i for i in remaining if re.match(r'.+\.[^.]+', i) and not i.endswith('.a') and not i.startswith('-')]
     return args
 
 
@@ -31,7 +32,8 @@ class CMakeProject:
     def __init__(self, name=''):
         self._name = name
         self.deps = {}
-        self.targets = []
+        self.defines = {}
+        self.targets = {}
         self.standards = set()
         self.include_dirs = set()
         self.other_flags = []
@@ -41,18 +43,18 @@ class CMakeProject:
     def name(self):
         if self._name:
             return self._name
-        return next(iter(i.name for i in self.targets), 'project_name')
+        return next(iter(self.targets), 'project_name')
 
-    def get_sources(self, target):
-        if target not in self.deps:
+    def get_leaves(self, target, source):
+        if not isinstance(target, str) or target not in source:
             return [target]
-        return sum([self.get_sources(i) for i in self.deps[target]], [])
+        return sum([self.get_leaves(i, source) for i in source[target]], [])
 
     def ingest(self, line):
-        if 'g++' in line:
-            line = line.split('g++')[-1].strip()
-        elif 'clang++' in line:
-            line = line.split('clang++')[-1].strip()
+        for tool in ['g++', 'gcc', 'clang++', 'clang']:
+            if tool in line:
+                line = line.split(tool)[-1].strip()
+                break
         else:
             return
         args = parse_compiler_args(line.split(' '))
@@ -64,19 +66,29 @@ class CMakeProject:
             self.standards.add(args.standard)
         if args.compile_only:
             if args.output:
-                assert len(args.inputs) == 1
+                assert len(args.inputs) == 1, 'Too many inputs: ' + str(args.inputs)
                 self.deps[args.output] = [args.inputs[0]]
+                self.defines[args.output] = [args.defines or []]
             else:
                 for filename in args.inputs:
-                    self.deps[splitext(filename)[0] + '.o'] = [filename]
+                    obj = splitext(filename)[0] + '.o'
+                    self.deps[obj] = [filename]
+                    self.defines[obj] = [args.defines or []]
         else:
             exe = args.output or 'a.out'
             self.deps[exe] = args.inputs
-            target = CMakeTarget(exe, self.get_sources(exe), args.libs)
+            parts = [args.defines or []]
+            for f in args.inputs:
+                if f in self.defines:
+                    parts += [f]
+            self.defines[exe] = parts
+            sources = self.get_leaves(exe, self.deps)
+            defines = sum(self.get_leaves(exe, self.defines), [])
+            target = CMakeTarget(exe, sources, defines, args.libs)
             if args.pthread:
                 target.libs.append('${CMAKE_THREAD_LIBS_INIT}')
                 self.pthread = True
-            self.targets.append(target)
+            self.targets[target.name] = target
 
     def __str__(self):
         parts = [
@@ -98,6 +110,6 @@ class CMakeProject:
             parts.append('find_package(Threads)')
         if self.include_dirs:
             parts.append('include_directories({})'.format(format_multiline(sorted(self.include_dirs))))
-        for target in self.targets:
+        for target in self.targets.values():
             parts.append(str(target))
         return '\n\n'.join(parts)
