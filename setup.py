930ef30833dd76069e207ab4284dd7f42499b5a7
Matthew D. Scholefield <matthew331199@gmail.com>
2019-12-04 21:47:28 -0600

    Increment version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='corappo',
-    version='0.1.0',
+    version='0.2.0',
     description='A tool to automatically generate CMake projects',
     url='https://github.com/matthewscholefield/corappo',
     author='Matthew D. Scholefield',
