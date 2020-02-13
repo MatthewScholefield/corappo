69ab73fdf5ec2a0dcf85d71ca4b899c6f3135cad
Matthew D. Scholefield <matthew331199@gmail.com>
2020-02-13 13:26:33 -0600

    Support library targets

--- corappo/cmake_target.py
+++ corappo/cmake_target.py
@@ -10,13 +10,19 @@ class CMakeTarget:
         self.flags = flags or []
 
     def __str__(self):
-        parts = ['add_executable({})'.format(format_multiline([self.name] + self.sources))]
+        flags = list(self.flags)
+        parts = []
+        if '-shared' in flags:
+            flags.remove('-shared')
+            parts += ['add_library({})'.format(format_multiline([self.name] + ['SHARED'] + self.sources))]
+        else:
+            parts += ['add_executable({})'.format(format_multiline([self.name] + self.sources))]
         if self.libs:
             parts += ['target_link_libraries({})'.format(format_multiline([self.name] + self.libs))]
         if self.defines:
             args = [self.name] + ['PUBLIC'] + self.defines
             parts += ['target_compile_definitions({})'.format(format_multiline(args))]
-        if self.flags:
-            args = [self.name] + ['PRIVATE'] + [i + ';' for i in self.flags]
+        if flags:
+            args = [self.name] + ['PRIVATE'] + [i + ';' for i in flags]
             parts += ['target_compile_options({})'.format(format_multiline(args))]
         return '\n\n'.join(parts)
